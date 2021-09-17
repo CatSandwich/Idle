5107a32c17ba950bd4c2d17a8e64409b57107dc9
CatSandwich <jtorrington2@gmail.com>
2021-09-17 13:36:25 -0400

    Add generic label script

---
+++ Assets/Scripts/UI/GenericLabel.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GenericLabel : MonoBehaviour
+{
+    public TextMeshProUGUI TMP;
+    [TextArea]
+    public string Text;
+
+    public void Start()
+    {
+        Stats.StatChanged += stats => _updateText();
+    }
+
+    private void OnValidate()
+    {
+        _updateText();
+    }
+
+    void _updateText()
+    {
+        if (!TMP) return;
+        
+        TMP.text = Text
+            .Replace("{{ Food }}", Stats.Food.Value.ToString())
+            .Replace("{{ Wood }}", Stats.Wood.Value.ToString())
+            .Replace("{{ Stone }}", Stats.Stone.Value.ToString())
+            .Replace("{{ BasicHouses }}", Stats.BasicHouses.Value.ToString())
+            .Replace("{{ BasicHouseCostWood }}", Stats.BasicHouseCost.wood.ToString())
+            .Replace("{{ BasicHouseCostStone }}", Stats.BasicHouseCost.stone.ToString());
+    }
+}
