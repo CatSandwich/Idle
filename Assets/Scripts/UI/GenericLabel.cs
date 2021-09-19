22e97bc606076646a5ccdece0e160b1182019da1
CatSandwich <jtorrington2@gmail.com>
2021-09-19 10:37:10 -0400

    Major stats refactor

--- Assets/Scripts/UI/GenericLabel.cs
+++ Assets/Scripts/UI/GenericLabel.cs
@@ -12,14 +12,14 @@ namespace UI
 
         public void Start()
         {
-            Stats.StatChanged += stats => _updateText();
-            GameManager.Instance.ActionChanged += action => _updateText();
+            Stats.StatChanged += _updateText;
+            GameManager.Instance.ActionChanged += _updateText;
             _updateText();
         }
 
         private void OnValidate() => _updateText();
 
-        void _updateText()
+        private void _updateText()
         {
             if (!TMP)
             {
@@ -29,26 +29,8 @@ namespace UI
                 return;
             }
 
-            try
-            {
-                TMP.text = Text
-                    .Replace("{{ Food }}", Stats.Food.Value.ToString())
-                    .Replace("{{ Wood }}", Stats.Wood.Value.ToString())
-                    .Replace("{{ Stone }}", Stats.Stone.Value.ToString())
-                    .Replace("{{ Gold }}", Stats.Food.Value.ToString())                 // HERE 
-                    .Replace("{{ Diamonds }}", Stats.Food.Value.ToString())  
-                    .Replace("{{ BasicHouses }}", Stats.BasicHouses.Value.ToString())
-                    .Replace("{{ Housing }}", Stats.Housing.ToString())
-                    .Replace("{{ Population }}", Stats.Population.Value.ToString())
-                    .Replace("{{ AvailablePopulation }}", Stats.AvailablePopulation.ToString())
-                    .Replace("{{ BasicHouseCostWood }}", Stats.BasicHouseCost.wood.ToString())
-                    .Replace("{{ BasicHouseCostStone }}", Stats.BasicHouseCost.stone.ToString())
-                    .Replace("{{ Action }}", GameManager.Instance.Action.ToString());
-            }
-            catch (Exception e)
-            {
-                Debug.LogException(e, gameObject);
-            }
+            try { TMP.text = Stats.ReplaceVariables(Text); }
+            catch (Exception e) { Debug.LogException(e, gameObject); }
         }
     }
 }
